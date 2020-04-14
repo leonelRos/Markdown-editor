736d1decef13c95e8127358676ad7ec3c0c08151
Leonel Rosales <rleonel426@gmail.com>
2020-04-13 18:34:45 -0700

    heroku deployed

--- server.js
+++ server.js
@@ -15,8 +15,28 @@ app.get('/', function(req,res) {
 //     res.render('pad')
 // })
 
+// Normalize a port into a number, string, or false.
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
+  
 
 //port listening in 3000
+var port = normalizePort(process.env.PORT || 3000);
+app.set('port', port);
+
 app.listen(3000);
 
 module.exports = app;
